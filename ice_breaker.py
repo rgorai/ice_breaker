62cdab5b4145d0ef55cfdc52399905c1d0ddf201
Ron Gorai <ron.gorai2020@gmail.com>
2023-06-15 18:12:47 -0400

    completed full stack application

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,30 +1,48 @@
+from typing import Tuple
 from dotenv import load_dotenv
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from agents.linkedin_lookup_agent import linkedin_lookup_agent
-
+import json
 from third_parties.linkedin import scrape_linkedin_profile
+from output_parsers import PersonIntel, person_intel_parser
 
 load_dotenv()
 
-if __name__ == "__main__":
-    SUMMARY_TEMPLATE = """
-                    given the LinkedIn information {information} about a person from I want you to create:
-                    1. a short summary
-                    2. two interesting facts about them
-                    """
 
-    linkedin_profile_url = linkedin_lookup_agent(name="Lauren Waletzki")
+def ice_break(name="") -> Tuple[PersonIntel, str]:
+    SUMMARY_TEMPLATE = """
+        given the LinkedIn information {information} about a person from I want you to create:
+        1. a short summary
+        2. 5 interesting facts about them
+        3. a topic that may interest them
+        4. 2 creative ice breakers to open a converstaion with them
+        
+        {format_instructions}
+    """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=SUMMARY_TEMPLATE
+        input_variables=["information"],
+        template=SUMMARY_TEMPLATE,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url)
+    # Switch between ProxyCurl and JSON data
+    linkedin_data = json.load(open("./linkedin_response.json"))
+    # linkedin_profile_url = linkedin_lookup_agent(name)
+    # linkedin_data = scrape_linkedin_profile(linkedin_profile_url)
 
-    print(chain.run(information=linkedin_data))
+    result = chain.run(information=linkedin_data)
+
+    return person_intel_parser.parse(result), linkedin_data.get("profile_pic_url")
+
+
+if __name__ == "__main__":
+    print(ice_break())
---
+++ output_parsers.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, Field
+from langchain.output_parsers import PydanticOutputParser
+from typing import List
+
+
+class PersonIntel(BaseModel):
+    summary: str = Field(description="summary of the person")
+    facts: List[str] = Field(description="interesting facts about the person")
+    topics_of_interest: List[str] = Field(
+        description="topics that may interest the person"
+    )
+    ice_breakers: List[str] = Field(
+        description="create ice breakers to open a conversation with the person"
+    )
+
+    def to_dict(self):
+        return {
+            "summary": self.summary,
+            "facts": self.facts,
+            "topics_of_interest": self.topics_of_interest,
+            "ice_breakers": self.ice_breakers,
+        }
+
+
+person_intel_parser = PydanticOutputParser(pydantic_object=PersonIntel)
